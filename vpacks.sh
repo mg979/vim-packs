9c4c66cdaa6b43d298c7c0efdd843b8d762b2bab
mg979 <mg1979.git@gmail.com>
2020-06-18 22:49:33 +0200

    fix: various to logging and errors display

--- vpacks.sh
+++ vpacks.sh
@@ -13,11 +13,15 @@ __vpacks_completions()
 		COMPREPLY=($(compgen -W "list update status verbose web git exe log logs move2opt move2start remotes fetch graph helptags restore backup terminal install" "${COMP_WORDS[1]}"))
 	elif [ "${#COMP_WORDS[@]}" == "3" ]; then
 		if [ "${COMP_WORDS[1]}" == "restore" ]; then
-			local backups=$(echo $vimdir/pack/backup/* | sed "s@$vimdir/pack/backup/@@g")
+			local backups=$(printf "%s\n" $vimdir/pack/backup/* | sed "s@$vimdir/pack/backup/@@g")
 			COMPREPLY=($(compgen -W "$backups" "${COMP_WORDS[2]}"))
+		elif [ "${COMP_WORDS[1]}" == "logs" ]; then
+			local packdirs=$(printf "%s\n" $vimdir/pack/* | grep -Ev 'backups|logs' | sed "s@$vimdir/pack/@@g")
+			local packs=$(printf "%s\n" $vimdir/pack/*/*/* | grep -E 'start|opt' | sed "s@$vimdir/pack/[^/]\+/[^/]\+/@@g")
+			COMPREPLY=($(compgen -W "errors clear clearall $packdirs $packs" "${COMP_WORDS[2]}"))
 		else
-			local packdirs=$(echo $vimdir/pack/* | sed "s@$vimdir/pack/@@g")
-			local packs=$(echo $vimdir/pack/*/*/* | sed "s@$vimdir/pack/[^/]\+/[^/]\+/@@g")
+			local packdirs=$(printf "%s\n" $vimdir/pack/* | grep -Ev 'backups|logs' | sed "s@$vimdir/pack/@@g")
+			local packs=$(printf "%s\n" $vimdir/pack/*/*/* | grep -E 'start|opt' | sed "s@$vimdir/pack/[^/]\+/[^/]\+/@@g")
 			COMPREPLY=($(compgen -W "$packdirs $packs" "${COMP_WORDS[2]}"))
 		fi
 	else
